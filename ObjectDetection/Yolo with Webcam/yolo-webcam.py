a0d84d79ea12fb56a296b43660a66cd6c07826ae
d3nnyyy <d3nnn41k@gmail.com>
2023-10-17 16:23:08 +0300

    added confidence and class name to be displayed along with the rectangle

--- ObjectDetection/Yolo with Webcam/yolo-webcam.py
+++ ObjectDetection/Yolo with Webcam/yolo-webcam.py
@@ -1,3 +1,5 @@
+import math
+
 from ultralytics import YOLO
 import cv2 as cv
 import cvzone
@@ -10,6 +12,18 @@ cap.set(4, 720)
 
 model = YOLO("../Yolo-Weights/yolov8n.pt")
 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
 while True:
 
     success, img = cap.read()
@@ -18,7 +32,6 @@ while True:
     for r in results:
         boxes = r.boxes
         for box in boxes:
-
             # Bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -26,6 +39,16 @@ while True:
 
             w, h = x2 - x1, y2 - y1
             cvzone.cornerRect(img, (x1, y1, w, h))
+
+            # Confidence
+
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+
+            # Class Name
+            cls = int(box.cls[0])
+
+            cvzone.putTextRect(img, f'{classNames[cls]} {confidence}', (max(0, x1), max(350, y1)), scale=0.7,
+                               thickness=1)
 
     cv.imshow("Image", img)
     cv.waitKey(1)
