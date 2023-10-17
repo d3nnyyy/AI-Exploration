556a72e507f118247dd2623166bf1e2163ee0164
d3nnyyy <d3nnn41k@gmail.com>
2023-10-17 20:52:54 +0300

    refactored and reorganized code by removing commented lines and adding comments

--- ObjectDetection/Car Counter/car-counter.py
+++ ObjectDetection/Car Counter/car-counter.py
@@ -1,15 +1,17 @@
 import math
 
-import cv2
 from ultralytics import YOLO
 import cv2 as cv
 import cvzone
 from sort import *
 
-cap = cv.VideoCapture("../Videos/cars.mp4")  # Video
+# Initialize the video capture with the path to the video file.
+cap = cv.VideoCapture("../Videos/cars.mp4")
 
+# Initialize the YOLO model with the pre-trained weights.
 model = YOLO("../Yolo-Weights/yolov8n.pt")
 
+# Define a list of class names for object detection.
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -22,23 +24,27 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+# Load a mask image for region of interest.
 mask = cv.imread("mask.png")
 
-# Tracking
-
+# Initialize a SORT tracker.
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
+# Define region of interest limits and an empty list to track detected objects.
 limits = [340, 350, 673, 350]
 total_count = []
 
 while True:
 
+    # Read a frame from the video.
     success, img = cap.read()
     imgRegion = cv.bitwise_and(img, mask)
 
+    # Overlay the car counter image on the frame.
     imgGraphics = cv.imread("graphics.png", cv.IMREAD_UNCHANGED)
     img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
 
+    # Perform object detection using YOLO.
     results = model(imgRegion, stream=True)
 
     detections = np.empty((0, 5))
@@ -47,55 +53,47 @@ while True:
         boxes = r.boxes
         for box in boxes:
 
-            # Bounding box
+            # Extract bounding box coordinates.
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
-
             w, h = x2 - x1, y2 - y1
 
-            # Confidence
+            # Calculate confidence and class name.
             confidence = math.ceil((box.conf[0] * 100)) / 100
-
-            # Class Name
             cls = int(box.cls[0])
             currentClass = classNames[cls]
 
-            if confidence > 0.3 \
-                    and currentClass == "car" \
-                    or currentClass == "truck" \
-                    or currentClass == "motorbike" \
-                    or currentClass == "bus" \
-                    or currentClass == "bicycle":
-                # cvzone.putTextRect(img, f'{classNames[cls]} {confidence}', (max(0, x1), max(350, y1)), scale=0.7,
-                #                    thickness=1)
-                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+            # Check if the detected object is a car, truck, motorbike, bus, or bicycle with confidence above 0.3.
+            if confidence > 0.3 and currentClass in ["car", "truck", "motorbike", "bus", "bicycle"]:
                 currentArray = np.array([x1, y1, x2, y2, confidence])
                 detections = np.vstack((detections, currentArray))
 
+    # Update object tracking with SORT.
     results_tracker = tracker.update(detections)
 
+    # Draw a line and track objects within specified limits.
     cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
 
     for result in results_tracker:
+
         x1, y1, x2, y2, id = result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        print(result)
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
-        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)), scale=2,
-                           thickness=3, offset=10)
 
+        # Draw a rectangle and label the tracked object.
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)), scale=2, thickness=3, offset=10)
+
+        # Calculate the center of the object.
         cx, cy = x1 + w // 2, y1 + h // 2
 
+        # Count and mark objects that cross the specified limits.
         if limits[0] < cx < limits[2] and limits[1] - 30 < cy < limits[1] + 30:
             if total_count.count(id) == 0:
                 total_count.append(id)
                 cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
 
-    # cvzone.putTextRect(img, f' Cars found: {len(total_count)}', (50, 50), scale=2,
-    #                    thickness=3, offset=10)
-    cv2.putText(img, str(len(total_count)), (255, 100), cv.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
+    # Display the count of cars found.
+    cv.putText(img, str(len(total_count)), (255, 100), cv.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
 
-    cv.imshow("MASK", imgRegion)
     cv.imshow("Image", img)
     cv.waitKey(1)
