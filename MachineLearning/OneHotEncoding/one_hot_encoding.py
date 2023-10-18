c8020666bc9587f7b07d6d861ae1325c2a63f8e3
d3nnyyy <d3nnn41k@gmail.com>
2023-10-18 19:42:27 +0300

    used dummy variables with sklearn.preprocessing.OneHotEncoder library

--- MachineLearning/OneHotEncoding/one_hot_encoding.py
+++ MachineLearning/OneHotEncoding/one_hot_encoding.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 df = pd.read_csv("homeprices.csv")
 
@@ -16,8 +19,29 @@ y = final.price
 
 model.fit(X, y)
 
-print(model.predict([[2800, 0, 1]]))
-print(model.predict([[3400, 1, 0]]))
-print(model.predict([[4000, 0, 0]]))
+# print(model.predict([[2800, 0, 1]]))
+# print(model.predict([[3400, 1, 0]]))
+# print(model.predict([[4000, 0, 0]]))
+#
+# print(model.score(X,y))
 
-print(model.score(X,y))
+le = LabelEncoder()
+
+dfle = df
+
+dfle.town = le.fit_transform(dfle.town)
+
+print(dfle)
+
+X = dfle[['town', 'area']].values
+y = dfle.price
+
+ohe = OneHotEncoder()
+
+ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [0])], remainder='passthrough')
+X = ct.fit_transform(X)
+
+X = X[:, 1:]
+
+model.fit(X,y)
+print(model.predict([[1, 0, 2800]]))
