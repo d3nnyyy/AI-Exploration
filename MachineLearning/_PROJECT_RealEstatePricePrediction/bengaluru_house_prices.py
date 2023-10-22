4e429c97983dda8265af5e35ff744684b7988add
d3nnyyy <d3nnn41k@gmail.com>
2023-10-22 10:58:24 +0300

    performed the outlier removal

--- MachineLearning/_PROJECT_RealEstatePricePrediction/bengaluru_house_prices.py
+++ MachineLearning/_PROJECT_RealEstatePricePrediction/bengaluru_house_prices.py
@@ -57,3 +57,82 @@ location_stats_less_than_10 = location_stats[location_stats <= 10]
 df5.location = df5.location.apply(lambda x: 'other' if x in location_stats_less_than_10 else x)
 
 print(len(df5.location.unique()))
+
+print(df5[df5.total_sqft / df5.bhk < 300].head())
+print(df5.shape)
+
+df6 = df5[~(df5.total_sqft / df5.bhk < 300)]
+
+print(df6.shape)
+
+print(df6.price_per_sqft.describe())
+
+
+def remove_pps_outliers(df):
+    df_out = pd.DataFrame()
+    for key, subdf in df.groupby('location'):
+        m = np.mean(subdf.price_per_sqft)
+        st = np.std(subdf.price_per_sqft)
+        reduced_df = subdf[(subdf.price_per_sqft > (m - st)) & (subdf.price_per_sqft <= (m + st))]
+        df_out = pd.concat([df_out, reduced_df], ignore_index=True)
+    return df_out
+
+
+df7 = remove_pps_outliers(df6)
+
+print(df7.shape)
+
+
+def plot_scatter_chart(df, location):
+    bhk2 = df[(df.location == location) & (df.bhk == 2)]
+    bhk3 = df[(df.location == location) & (df.bhk == 3)]
+    matplotlib.rcParams['figure.figsize'] = (15, 10)
+    plt.scatter(bhk2.total_sqft, bhk2.price, color='blue', label='2 BHK', s=50)
+    plt.scatter(bhk3.total_sqft, bhk3.price, marker='+', color='green', label='3 BHK', s=50)
+    plt.xlabel("Total Square Feet Area")
+    plt.ylabel("Price (Lakh Indian Rupees)")
+    plt.title(location)
+    plt.legend()
+    plt.show()
+
+
+# plot_scatter_chart(df7, "Hebbal")
+
+
+def remove_bhk_outliers(df):
+    exclude_indices = np.array([])
+    for location, location_df in df.groupby('location'):
+        bhk_stats = {}
+        for bhk, bhk_df in location_df.groupby('bhk'):
+            bhk_stats[bhk] = {
+                'mean': np.mean(bhk_df.price_per_sqft),
+                'std': np.std(bhk_df.price_per_sqft),
+                'count': bhk_df.shape[0]
+            }
+        for bhk, bhk_df in location_df.groupby('bhk'):
+            stats = bhk_stats.get(bhk - 1)
+            if stats and stats['count'] > 5:
+                exclude_indices = np.append(exclude_indices,
+                                            bhk_df[bhk_df.price_per_sqft < (stats['mean'])].index.values)
+    return df.drop(exclude_indices, axis='index')
+
+
+df8 = remove_bhk_outliers(df7)
+
+# plot_scatter_chart(df8, "Hebbal")
+
+
+# plt.hist(df8.price_per_sqft, rwidth=0.8)
+# plt.xlabel("Price Per Square Feet")
+# plt.ylabel("Count")
+# plt.show()
+
+# plt.hist(df8.bath, rwidth=0.8)
+# plt.xlabel("Number of bathrooms")
+# plt.ylabel("Count")
+# plt.show()
+
+df9 = df8[df8.bath > df8.bhk + 2]
+
+df10 = df9.drop(['size', 'price_per_sqft'], axis='columns')
+print(df10.head(3))
