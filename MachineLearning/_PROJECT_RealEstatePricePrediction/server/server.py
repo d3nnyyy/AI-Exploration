84c012076b500adc91c7a6917fa188b881efb3fc
d3nnyyy <d3nnn41k@gmail.com>
2023-10-22 12:50:13 +0300

    implemented predict_house_price endpoint

--- MachineLearning/_PROJECT_RealEstatePricePrediction/server/server.py
+++ MachineLearning/_PROJECT_RealEstatePricePrediction/server/server.py
@@ -14,6 +14,22 @@ def get_location_names():
     return response
 
 
+@app.route("/predict_house_price", methods=['POST'])
+def predict_house_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
