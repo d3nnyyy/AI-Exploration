1d39333a1602da6d3aaeca31d05d080ec66329dd
d3nnyyy <d3nnn41k@gmail.com>
2023-10-29 15:07:53 +0200

    implemented gradient_descent from scratch

--- DeepLearning/GradientDescent/gradient_descent.py
+++ DeepLearning/GradientDescent/gradient_descent.py
@@ -40,3 +40,65 @@ model.evaluate(X_test_scaled, y_test)
 coef, intercept = model.get_weights()
 
 print(coef, intercept)
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+print(sigmoid(18))
+
+
+def prediction_function(age, affordability):
+    weighted_sum = coef[0] * age + coef[1] * affordability + intercept
+    return sigmoid(weighted_sum)
+
+
+print(prediction_function(0.47, 1))
+
+
+def log_loss(y_true, y_predicted):
+    epsilon = 1e-15
+    y_predicted_eps = [max(i, epsilon) for i in y_predicted]
+    y_predicted_eps = [min(i, 1 - epsilon) for i in y_predicted_eps]
+    y_predicted_eps = np.array(y_predicted_eps)
+    return -np.mean(y_true * np.log(y_predicted_eps) + (1 - y_true) * np.log(1 - y_predicted_eps))
+
+
+def sigmoid_numpy(X):
+    return 1 / (1 + np.exp(-X))
+
+
+print(sigmoid_numpy(np.array([12, 0, 1])))
+
+
+def gradient_descent(age, affordability, y_true, epochs, loss_threshold):
+    # w1, w2, bias
+    w1 = w2 = 1
+    bias = 0
+    rate = 0.5
+    n = len(age)
+
+    for i in range(epochs):
+        weighted_sum = w1 * age + w2 * affordability + bias
+        y_predicted = sigmoid_numpy(weighted_sum)
+        loss = log_loss(y_true, y_predicted)
+
+        w1d = (1 / n) * np.dot(np.transpose(age), (y_predicted - y_true))
+        w2d = (1 / n) * np.dot(np.transpose(affordability), (y_predicted - y_true))
+
+        bias_d = np.mean(y_predicted - y_true)
+
+        w1 = w1 - rate * w1d
+        w2 = w2 - rate * w2d
+        bias = bias - rate * bias_d
+
+        print(f'Epoch:{i}, w1:{w1}, w2:{w2}, bias:{bias}, loss:{loss}')
+
+        if loss < loss_threshold:
+            break
+
+    return w1, w2, bias
+
+
+print(gradient_descent(X_train_scaled['age'], X_train_scaled['affordability'], y_train, 1000, 0.4631))
