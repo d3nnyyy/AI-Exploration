8b4e7ea335dca9425f3849483c244c555fed22cf
d3nnyyy <d3nnn41k@gmail.com>
2023-10-29 16:15:28 +0200

    implemented batch_gradient_descent

---
+++ DeepLearning/SGDvsGD/sgd_and_gd.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn import preprocessing
+
+df = pd.read_csv("homeprices_bengaluru.csv")
+
+sx = preprocessing.MinMaxScaler()
+sy = preprocessing.MinMaxScaler()
+
+scaled_X = sx.fit_transform(df.drop('price', axis='columns'))
+scaled_y = sy.fit_transform(df['price'].values.reshape(df.shape[0], 1))
+
+
+def batch_gradient_descent(X, y_true, epochs, learning_rate=0.01):
+    number_of_features = X.shape[1]
+
+    w = np.ones(shape=number_of_features)
+    bias = 0
+    total_samples = X.shape[0]
+
+    cost_list = []
+    epoch_list = []
+
+    for i in range(epochs):
+        y_predicted = np.dot(w, scaled_X.T) + bias
+
+        w_grad = -(2 / total_samples) * (X.T.dot(y_true - y_predicted))
+        b_grad = -(2 / total_samples) * np.sum(y_true - y_predicted)
+
+        w = w - learning_rate * w_grad
+        bias = bias - learning_rate * b_grad
+
+        cost = np.mean(np.square(y_true - y_predicted))
+
+        if i % 10 == 0:
+            cost_list.append(cost)
+            epoch_list.append(i)
+
+    return w, bias, cost, cost_list, epoch_list
+
+
+w, bias, cost, cost_list, epoch_list = batch_gradient_descent(scaled_X, scaled_y.reshape(scaled_y.shape[0], ), 500)
+print(w, bias, cost)
+
+plt.xlabel("epoch")
+plt.ylabel("cost")
+plt.plot(epoch_list, cost_list)
+
+
+# plt.show()
+
+def predict(area, bedrooms, w, b):
+    scaled_X = sx.transform([[area, bedrooms]])[0]
+    scaled_price = w[0] * scaled_X[0] + w[1] * scaled_X[1] + b
+    return sy.inverse_transform([[scaled_price]])[0][0]
+
+
+print(predict(2600, 4, w, bias))
